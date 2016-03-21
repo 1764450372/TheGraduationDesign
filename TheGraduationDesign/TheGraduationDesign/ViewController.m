8614c95cc5e5a6b9c52804838a594f82514c8516
zhao <984379778@qq.com>
2016-03-21 21:27:32 +0800

    this commit text

--- TheGraduationDesign/TheGraduationDesign/ViewController.m
+++ TheGraduationDesign/TheGraduationDesign/ViewController.m
@@ -16,6 +16,8 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    NSString * start = @"start";
+    NSLog(@"%@" , start);
     // Do any additional setup after loading the view, typically from a nib.
 }
 
